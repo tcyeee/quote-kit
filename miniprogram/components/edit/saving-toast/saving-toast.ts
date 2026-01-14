8d3459357f0f3b07e4a63deeeba2c5244e4a5899
tcyeee <tcyeee@outlook.com>
2026-01-14 17:42:01 +0800

    fix(saving-toast): improve visibility and duration handling

--- miniprogram/components/edit/saving-toast/saving-toast.ts
+++ miniprogram/components/edit/saving-toast/saving-toast.ts
@@ -1,25 +1,38 @@
-let savingToastTimer: any = null
-
 Component({
   data: {
     visible: false,
+    topPadding: 0,
+    duration: 1800,
+  },
+  lifetimes: {
+    attached() {
+      const info = wx.getSystemInfoSync()
+      const safeTop =
+        (info as any).safeAreaInsets?.top ??
+        (info as any).safeArea?.top ??
+        info.statusBarHeight ??
+        0
+      this.setData({
+        topPadding: safeTop,
+      })
+    },
   },
   methods: {
     show() {
-      if (savingToastTimer) {
-        clearTimeout(savingToastTimer)
-        savingToastTimer = null
+      const self = this as any
+      if (self._savingToastTimer) {
+        clearTimeout(self._savingToastTimer)
+        self._savingToastTimer = null
       }
       this.setData({
         visible: true,
       })
-      savingToastTimer = setTimeout(() => {
+      self._savingToastTimer = setTimeout(() => {
         this.setData({
           visible: false,
         })
-        savingToastTimer = null
-      }, 500)
+        self._savingToastTimer = null
+      }, this.data.duration)
     },
   },
 })
-
