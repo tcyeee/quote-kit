259c7062a2968f17bcfa525d771e65f067d8ed71
tcyeee <tcyeee@outlook.com>
2026-01-18 16:42:30 +0800

    feat(preview): redesign deep-sea quote component with new layout and styles

--- miniprogram/components/preview/deep-sea/deep-sea.ts
+++ miniprogram/components/preview/deep-sea/deep-sea.ts
@@ -1,8 +1,20 @@
+import { calculateTotalAmount } from '../../../utils/quote-utils'
+
 Component({
   properties: {
     quoteDetail: {
       type: Object,
       value: {} as QuoteDetail,
     },
+  },
+  observers: {
+    quoteDetail(quoteDetail: QuoteDetail) {
+      if (!quoteDetail) return
+      const totalAmount = calculateTotalAmount(quoteDetail)
+      this.setData({ totalAmount })
+    },
+  },
+  data: {
+    totalAmount: 0,
   },
 })
