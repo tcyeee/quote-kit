dbefdac8f7673b6270c5cdf9df1dea3b78d78b80
tcyeee <tcyeee@outlook.com>
2026-01-26 22:03:05 +0800

    chore: update configs, add backend ping and adjust dependencies

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -11,5 +11,17 @@ App<IAppOption>({
     await getUserId()
     const quoteDetail = await getDefaultQuoteDetail()
     this.globalData.quoteDetail = quoteDetail as QuoteDetail
+    try {
+      await new Promise<WechatMiniprogram.RequestSuccessCallbackResult>((resolve, reject) => {
+        wx.request({
+          url: 'http://127.0.0.1:3000/ping',
+          method: 'GET',
+          success: resolve,
+          fail: reject,
+        })
+      })
+    } catch (error) {
+      console.error('调用后端 ping 接口失败', error)
+    }
   },
 })
